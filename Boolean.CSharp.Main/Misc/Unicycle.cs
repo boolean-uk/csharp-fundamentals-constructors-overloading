e583f738e1db92a0ec49df8e4f04278fbcaa97ea
Stian Gaustad <stian.gaustad@no.experis.com>
2024-01-10 10:31:04 +0100

    Solved core

--- Boolean.CSharp.Main/Misc/Unicycle.cs
+++ Boolean.CSharp.Main/Misc/Unicycle.cs
@@ -10,6 +10,11 @@ namespace Boolean.CSharp.Main.Misc
     {
         private string _nameOfUnicyclist;
 
+        public Unicycle(string riderName)
+        {
+            _nameOfUnicyclist = riderName;
+        }
+
         public string NameOfUnicyclist { get => _nameOfUnicyclist; set => _nameOfUnicyclist = value; }
         public int WheelCount { get; set; } = 1;
     }
