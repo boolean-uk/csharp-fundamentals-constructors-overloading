09895df2352ce852fb4db0d2529c7e527c3d4b32
Kanthee Kosolyuthasarn <Kanthee>
2024-01-10 15:14:09 +0100

    Kanthee Kosolyuthasarn - C#3 - 10.01.2024

--- Boolean.CSharp.Main/Misc/Bicycle.cs
+++ Boolean.CSharp.Main/Misc/Bicycle.cs
@@ -9,7 +9,10 @@ namespace Boolean.CSharp.Main.Misc
     public class Bicycle
     {
         private int _wheelCount;
-
-        public int WheelCount { get; set; }
+        public Bicycle(int x)
+        {
+            _wheelCount = x;
+        }
+        public int WheelCount { get { return _wheelCount; } set { } }
     }
 }
