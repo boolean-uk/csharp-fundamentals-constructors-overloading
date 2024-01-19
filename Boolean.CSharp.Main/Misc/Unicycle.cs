316d33735a058fe49c558f50e75282d6268c3552
Linus Willmont <linus.willmont@se.experis.com>
2024-01-19 19:58:38 +0100

    Done

--- Boolean.CSharp.Main/Misc/Unicycle.cs
+++ Boolean.CSharp.Main/Misc/Unicycle.cs
@@ -10,6 +10,11 @@ namespace Boolean.CSharp.Main.Misc
     {
         private string _nameOfUnicyclist;
 
+        public Unicycle()
+        {
+            _nameOfUnicyclist = "Linus";
+        }
+
         public string NameOfUnicyclist { get => _nameOfUnicyclist; set => _nameOfUnicyclist = value; }
         public int WheelCount { get; set; } = 1;
     }
