0f4bbe6b84422732f53823da8f8f9ba9b984acbb
Nikolai Lillemark Børseth <nikolailb1999@gmail.com>
2025-01-07 14:27:33 +0100

    Passed all tests

--- Boolean.CSharp.Main/Extension.cs
+++ Boolean.CSharp.Main/Extension.cs
@@ -7,23 +7,59 @@ using System.Reflection.Metadata.Ecma335;
 namespace Boolean.CSharp.Main
 {
     public class Extension
-    {        
+    {
 
         //Implement the following methods:
 
         //TODO: 1. add, which accepts two floats and returns a float (both floats added together)
+        public float add(float a, float b) {
+            return a + b;
+        }
 
         //TODO: 2. add, which accepts two doubles and returns a double (both doubles added together)
+        public double add(double a, double b)
+        {
+            return a + b;
+        }
 
         //TODO: 3. subtract, which accepts two floats and returns a float (first float minus second float)      
+        public float subtract(float a, float b)
+        {
+            return a - b;
+        }
 
         //TODO: 4. subtract, which accepts a String and a char and returns a string with all instances of the provided char removed        
+        public string subtract(string a, char b)
+        {
+            return a.Replace(char.ToString(b), "");
+        }
+
 
         //TODO: 5. multiply, which accepts two ints and returns an int (first int multiplied by second int)
+        public int multiply(int a, int b)
+        {
+            return a * b;
+        }
 
         //TODO: 6. multiply, which accepts a string and an int, and returns a string containing the provided string as many times as the provided int separated by a comma. E.g. multiply("Hello", 3) -> "Hello,Hello,Hello"
-      
+        public string multiply(string a, int b)
+        {
+            string result = a;
+            for (int i = 1; i < b; i++)
+            {
+                result += "," + a;
+            }
+            return result;
+        }
         //TODO: 7. multiply, which accepts an array of Strings that each contain a number, and an int. The method should return an array of ints that contain the value of multiplying each String number by the provided int E.g. multiply(["2", "7", "3"], 3) -> [6, 21, 9]
-
+        public int[] multiply(string[] a, int b)
+        {
+            int[] result = new int[a.Length];
+            for (int i = 0; i < a.Length; i++)
+            {
+                result[i] = b * Int32.Parse(a[i]);
+            }
+            return result;
+        }
     }
 }
